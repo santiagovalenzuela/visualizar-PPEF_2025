4786ab67283a7067d200d89d5901a18d25418f68
santiago <1673271+santiagovalenzuela@users.noreply.github.com>
2024-11-26 23:43:49 -0600

    Added comparison with PEF2024

--- viz_ppef25.R
+++ viz_ppef25.R
@@ -1,22 +1,45 @@
 rm(list=ls(all=T))
 
 library(showtext)
+font_add_google(name = "Ubuntu", family = "ubuntu")
 showtext_auto()
 
 library(tidyverse)
 library(treemapify)
 
-#Cargamos PPEF
+#Cargamos PPEF25 y PEF24
 ppef25 <-read_csv("PPEF_2025.csv")
+pef24 <-read_csv("PEF_2024.csv")
 
 # Agrupamos por ramo
-ramos <- ppef25 %>% group_by(DESC_RAMO) %>%
-  summarize(monto = sum(MONTO_PROYECTO))
+ramos25 <- ppef25 %>% group_by(DESC_RAMO) %>%
+  summarize(monto = sum(MONTO_PROYECTO, na.rm = TRUE))
+ramos25 <- ramos25 %>% ungroup()
 
-ramos <- ramos %>% ungroup()
+ramos24 <- pef24 %>% group_by(DESC_RAMO) %>%
+  summarize(monto = sum(MONTO_PEF_2024, na.rm = TRUE))
+ramos24 <- ramos24 %>% ungroup()
 
-#Creamos la gráfica
-grafica <-ggplot(ramos, aes(area = monto,
+#Comparamos el PPEF25 con el PEF24, añadiendo una columna con el cambio
+#porcentual en el ramo, y otra diciendo si bajó o subió
+
+comparativo <- ramos24 %>%
+  left_join(ramos25, by = "DESC_RAMO")
+
+comparativo <- comparativo %>%
+  #renombramos las columnas
+  rename(monto24 = monto.x,
+         monto25 = monto.y) %>%
+  #Vemos si el monto crecio, bajo o quedó igual
+  mutate(comparison = case_when(
+    monto25 > monto24 ~ "crecio",
+    monto25 < monto24 ~ "bajo",
+    TRUE ~ "igual"))
+
+#Creamos las gráficas
+
+# Grafica1 muestra únicamente cómo se divide el presupuesto
+grafica1 <-ggplot(ramos25, aes(area = monto,
                             label = DESC_RAMO)) +
   
   geom_treemap(color = "white",
@@ -35,8 +58,8 @@ grafica <-ggplot(ramos, aes(area = monto,
   
   coord_cartesian(expand = FALSE, clip = "off") +
   
-  labs(title="Esto (probablemente) gastará México el próximo año",
-       subtitle = "Cada cuadrado representa un ramo del Proyecto de Presupuesto de Egresos del 2025. Su tamaño es equivalente al monto proyectado.\nEl presupuesto definitivo se votará el 5 de diciembre del 2024",
+  labs(title="En esto gastará México el próximo año",
+       subtitle = "Cada rectángulo representa un ramo del Proyecto de Presupuesto de Egresos del 2025. Su tamaño es equivalente al monto proyectado.\nEl presupuesto definitivo se votará el 5 de diciembre del 2024",
        caption = "Fuente: Proyecto de Presupuesto de Egresos de la Federación, 2025") +
   
   theme(axis.text.x = element_blank(),
@@ -47,13 +70,14 @@ grafica <-ggplot(ramos, aes(area = monto,
         axis.title.y = element_blank(),
         text = element_text(family = "ubuntu"),
         legend.position = "none",
-        linewidth = 5,
         plot.title = element_text(hjust = 0,
                                   color = "#631100",
                                   family = "ubuntu",
                                   face = "bold",
                                   size = 36),
-        plot.caption = element_text(hjust = 0))
+        plot.subtitle = element_text(size = 18),
+        plot.caption = element_text(hjust = 0,
+                                    size = 12))
 
 
-grafica
+grafica1
